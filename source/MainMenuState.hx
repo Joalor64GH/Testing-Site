446de82cc32c21269b056adcbefcf5165378630c
Joalor64 <rhgarcon@gmail.com>
2022-07-16 12:54:52 -0400

    oops

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -244,9 +244,6 @@ class MainMenuState extends MusicBeatState
 
 		#if debug
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Joalor64 Engine " + debug, 12);
-		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
 		#end
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Joalor64 Engine v" + joalor64EngineVersion, 12);
 		versionShit.scrollFactor.set();
