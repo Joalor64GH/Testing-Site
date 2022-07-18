7b411bf6731a1b292746eb40b57d8fbab7109884
Joalor64 <rhgarcon@gmail.com>
2022-07-18 13:02:49 -0400

    Update DVDState.hx

--- source/DVDState.hx
+++ source/DVDState.hx
@@ -33,7 +33,7 @@ override function create() {
 }
 
 override function update(elapsed:Float) {
-    if (FlxG.state.controls.BACK) {
+    if (FlxG.keys.justPressed.BACK) {
         FlxG.switchState(new MainMenuState());
     }
     if (dvdLogo.x > FlxG.width - dvdLogo.width || dvdLogo.x < 0) {
